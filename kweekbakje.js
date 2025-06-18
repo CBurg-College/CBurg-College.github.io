ebe0e2327a193a388d2e88b43f3be9a398abc5fc
CBurg-College <cburgmicro@outlook.com>
2025-06-18 20:48:40 +0200

    Add files via upload

--- kweekbakje.js
+++ kweekbakje.js
@@ -1,16 +1,32 @@
+const apiKey = "KTSWPMNXMTF3ICCN";	// Read Key only needed in case of a private channel
 const channelId = "2936917";		// Channel number of 'Kweekbakje 1'
 const points = 1;					// Number of data points requested
-const url = `https://api.thingspeak.com/channels/${channelId}/feeds.json&results=${points}`;
+const url = `https://api.thingspeak.com/channels/${channelId}/feeds.json?api_key=${apiKey}&results=${points}`;
+
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
 	const feeds = data.feeds;
-	feeds.forEach(feed => {
-	  document.getElementById("Grondvochtigheid").innerHTML = `${feed.field1}`;
-	  document.getElementById("Lichtsterkte").innerHTML = `${feed.field2}`;
-	  document.getElementById("Luchtvochtigheid").innerHTML = `${feed.field3}`;
-	  document.getElementById("Temperatuur").innerHTML = `${feed.field4}`;
-	  document.getElementById("Pomp").innerHTML = `${feed.field5}`;
-	});
+	const moisture = document.getElementById("Grondvochtigheid")
+	const illumination = document.getElementById("Lichtsterkte")
+	const temperature = document.getElementById("Temperatuur")
+	const humidity = document.getElementById("Luchtvochtigheid")
+	const pump = document.getElementById("Pomp")
+	if (feeds && (feeds.length > 0)) {
+		feeds.forEach(feed => {
+		  if (moisture) moisture.innerHTML = `${feed.field1}`;
+		  if (illumination) illumination.innerHTML = `${feed.field2}`;
+		  if (humidity) humidity.innerHTML = `${feed.field3}`;
+		  if (temperature) temperature.innerHTML = `${feed.field4}`;
+		  if (pump) pump.innerHTML = `${feed.field5}`;
+		});
+	}
+	else {
+	  if (moisture) moisture.innerHTML = `###`;
+	  if (illumination) illumination.innerHTML = `###`;
+	  if (humidity) humidity.innerHTML = `###`;
+	  if (temperature) temperature.innerHTML = `###`;
+	  if (pump) pump.innerHTML = `###`;
+	}
   })
